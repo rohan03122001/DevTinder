a99cd2479fb0c2eb5d0b0cbd2efa4940270fca6e
Rohan Bhujbal <rohanbhujbal2001@gmail.com>
2025-08-13 16:27:44 +0100

    get Connections done

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,8 @@ const ConnectionRequestModel = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
+const SAFE_DATA = ["firstName", "lastName", "skills", "about"];
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -11,9 +13,26 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequestModel.find({
       toUserID: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserID", ["firstName", "lastName", "skills", "about"]);
+    }).populate("fromUserID", SAFE_DATA);
 
     res.json({ message: "Data sent Success", connectionRequests });
+  } catch (error) {
+    res.status(400).send("Error: " + error);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequestModel.find({
+      $or: [
+        { toUserID: loggedInUser._id, status: "accepted" },
+        { fromUserID: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserID toUserID", SAFE_DATA);
+
+    res.json({ message: "Data sent Success", connections });
   } catch (error) {
     res.status(400).send("Error: " + error);
   }
